f6eb1cc7070a94bd9bfda00b79b54547f6cc2a26
lisichka2022 <99811204+lisichka2022@users.noreply.github.com>
2023-01-26 20:29:22 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+let numb1=+prompt('Input first number');
+let numb2=+prompt('Inputsecond number');
+
+function multiply(numb1,numb2){
+    let result=numb1*numb2;
+    return result;
+}
+console.log(multiply(numb1,numb2));
+
+function add(numb1,numb2){
+    let result=numb1+numb2;
+    return result;
+}
+console.log(add(numb1,numb2));
+
+function sub(numb1,numb2){
+    let result=numb1-numb2;
+    return result;
+}
+console.log(sub(numb1,numb2));
